84cba53e28bcbc01c070ae59e6815dac8014ffe8
Ernor <Ernor@Ernor-PC>
2018-05-03 01:23:08 -0300

    3.6.Cadastrando nova categoria

--- MoneyApi/src/main/java/br/com/api/resource/CategoriaResource.java
+++ MoneyApi/src/main/java/br/com/api/resource/CategoriaResource.java
@@ -1,12 +1,21 @@
 package br.com.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.api.model.Categoria;
 import br.com.api.repository.CategoriaRepository;
@@ -27,6 +36,20 @@ public class CategoriaResource {
 	@GetMapping("/outros")
 	public String outro() {
 		return "OK";
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
+		return categoriaRepository.findById(codigo).get();
 	}
 	
 }
