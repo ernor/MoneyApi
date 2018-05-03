d62a1e14352676edff9016543c7c05b850fe3ac2
Ernor <Ernor@Ernor-PC>
2018-05-02 23:13:08 -0300

    3.4.Criando o mapeamento de Categoria

---
+++ MoneyApi/src/main/java/br/com/api/resource/CategoriaResource.java
@@ -0,0 +1,30 @@
+package br.com.api.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.model.Categoria;
+import br.com.api.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public List<Categoria> listar() {
+		return categoriaRepository.findAll();
+	}
+	
+	@GetMapping("/outros")
+	public String outro() {
+		return "OK";
+	}
+	
+}
