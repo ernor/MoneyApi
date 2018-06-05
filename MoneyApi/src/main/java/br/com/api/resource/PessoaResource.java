8aad9638ade40b7df8daa42c9f6f4d92ee8f05e3
Ernor <Ernor@Ernor-PC>
2018-06-04 22:28:08 -0300

    4.4.Atualizando Propriedade Ativa

--- MoneyApi/src/main/java/br/com/api/resource/PessoaResource.java
+++ MoneyApi/src/main/java/br/com/api/resource/PessoaResource.java
@@ -76,5 +76,11 @@ public class PessoaResource {
 		Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);	
 		return ResponseEntity.ok(pessoaSalva);
 	}
+	
+	@PutMapping("/{codigo}/ativo")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void atualizarPropriedadeAtiva(@PathVariable Long codigo, @RequestBody Boolean ativo){		
+		pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
+	}
 
 }
--- MoneyApi/src/main/java/br/com/api/service/PessoaService.java
+++ MoneyApi/src/main/java/br/com/api/service/PessoaService.java
@@ -15,6 +15,18 @@ public class PessoaService {
 	private PessoaRepository pessoaRepository;
 	
 	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+		Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+		return pessoaRepository.save(pessoaSalva);
+	}
+
+	public void atualizarPropriedadeAtivo(Long codigo, Boolean ativo) {
+		Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+		pessoaSalva.setAtivo(ativo);
+		pessoaRepository.save(pessoaSalva);
+	}
+	
+	private Pessoa buscarPessoaPeloCodigo(Long codigo) {
 		Pessoa pessoaSalva = null;
 		try {
 			pessoaSalva = pessoaRepository.findById(codigo).get();
@@ -24,9 +36,7 @@ public class PessoaService {
 		if(pessoaSalva == null) {
 			throw new EmptyResultDataAccessException(1);
 		}
-		
-		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
-		return pessoaRepository.save(pessoaSalva);
+		return pessoaSalva;
 	}
 	
 }
