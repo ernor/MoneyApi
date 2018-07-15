a9025b2fca35695e6b25108ad925c4e73d1d3850
Ernor <Ernor@Ernor-PC>
2018-07-15 01:44:07 -0300

    5.9.Paginacao Lancamentos

--- MoneyApi/src/main/java/br/com/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ MoneyApi/src/main/java/br/com/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,12 +1,13 @@
 package br.com.api.repository.lancamento;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import br.com.api.model.Lancamento;
 import br.com.api.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 	
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 
 }
--- MoneyApi/src/main/java/br/com/api/resource/LancamentoResource.java
+++ MoneyApi/src/main/java/br/com/api/resource/LancamentoResource.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -31,7 +33,7 @@ import br.com.api.service.LancamentoService;
 import br.com.api.service.exception.PessoaInexistenteOuInativaException;
 
 @RestController
-@RequestMapping("/lancamento")
+@RequestMapping("/lancamentos")
 public class LancamentoResource {
 	
 	@Autowired
@@ -54,10 +56,19 @@ public class LancamentoResource {
 	}
 	*/
 	
+	/*
 	@GetMapping
 	public List<Lancamento> pesquisar(LancamentoFilter lancamentoFilter) {
 		return lancamentoRepository.filtrar(lancamentoFilter);
 	}
+	*/
+	
+	@GetMapping
+	public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
+		return lancamentoRepository.filtrar(lancamentoFilter, pageable);
+	}
+	
+	
 	
 	@GetMapping("/{codigo}")
 	public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo) {
